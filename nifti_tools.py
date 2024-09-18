5c944ee5a6f36f6f4d07b46766bcfb70468d40f2
Baris Kanber <b.kanber@ucl.ac.uk>
2024-09-18 02:40:47 +0100

    Update save_nifti

--- nifti_tools.py
+++ nifti_tools.py
@@ -1,25 +1,24 @@
 import numpy as np
 import nibabel as nib
 
-debug=False
-
-def save_nifti(data,affine=np.eye(4),header=None,filename='save_nifti.nii.gz',zooms=None):
+def save_nifti(data, affine=np.eye(4), header=None, filename='save_nifti.nii.gz', zooms=None, verbose=False):
     if header is not None:
         # check/update cal_min/max
-        cal_min=np.min(data)
-        cal_max=np.max(data)
-        if 'cal_min' not in header or 'cal_max' not in header or header['cal_min']!=cal_min or header['cal_max']!=cal_max:
-            header['cal_min']=cal_min
-            header['cal_max']=cal_max
-            if debug:
+        cal_min = np.min(data)
+        cal_max = np.max(data)
+        if 'cal_min' not in header or 'cal_max' not in header or header['cal_min'] != cal_min or header['cal_max'] != cal_max:
+            header['cal_min'] = cal_min
+            header['cal_max'] = cal_max
+            if verbose:
                 print('Adjusted cal_min/max for '+filename)
 
-        nibobj=nib.Nifti1Image(data,affine,header)
+        nibobj = nib.Nifti1Image(data, affine, header)
     else:
-        nibobj=nib.Nifti1Image(data,affine)
+        nibobj = nib.Nifti1Image(data, affine)
+
+    nibobj.set_data_dtype(data.dtype)
 
     if zooms is not None:
-        nibobj.header.set_zooms(zooms) 
+        nibobj.header.set_zooms(zooms)
 
-    nib.save(nibobj,filename)
-
+    nib.save(nibobj, filename)
