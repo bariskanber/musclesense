74c07eaeb184212bc222c9af868364f06bb9be08
Baris Kanber <b.kanber@ucl.ac.uk>
2024-09-18 02:43:49 +0100

    Update mmseg_utils

--- mmseg_utils.py
+++ mmseg_utils.py
@@ -1,4 +1,5 @@
-import glob
+import sys
+from pathlib import Path
 import numpy as np
 from scipy import ndimage as nd
 
@@ -8,24 +9,26 @@ def numpy_dice_coefficient(y_true, y_pred, smooth=1.):
     intersection = np.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
-def scale2D(inmat,targetsize_y,targetsize_x,order=3,mode='constant',cval=0.0,prefilter=True):
-    z=[float(targetsize_y)/inmat.shape[0],float(targetsize_x)/inmat.shape[1]]
 
-    retmat = nd.interpolation.zoom(inmat,zoom=z,order=order,mode=mode,cval=cval,prefilter=prefilter)
+def scale2D(inmat, targetsize_y, targetsize_x, order=3, mode='constant', cval=0.0, prefilter=True):
+    z = [float(targetsize_y)/inmat.shape[0], float(targetsize_x)/inmat.shape[1]]
 
-    if retmat.shape!=(targetsize_y,targetsize_x):
-        print('scale2D did not return the expected size output',retmat.shape)
+    retmat = nd.interpolation.zoom(inmat, zoom=z, order=order, mode=mode, cval=cval, prefilter=prefilter)
+
+    if retmat.shape != (targetsize_y, targetsize_x):
+        print('scale2D did not return the expected size output', retmat.shape)
         raise Exception('scale2D error')
 
     return retmat
 
-def scale3D(inmat,targetsize_z,targetsize_y,targetsize_x,order=3,mode='constant',cval=0.0,prefilter=True):
-    z=[float(targetsize_z)/inmat.shape[0],float(targetsize_y)/inmat.shape[1],float(targetsize_x)/inmat.shape[2]]
 
-    retmat = nd.interpolation.zoom(inmat,zoom=z,order=order,mode=mode,cval=cval,prefilter=prefilter)
+def scale3D(inmat, targetsize_z, targetsize_y, targetsize_x, order=3, mode='constant', cval=0.0, prefilter=True):
+    z = [float(targetsize_z)/inmat.shape[0], float(targetsize_y)/inmat.shape[1], float(targetsize_x)/inmat.shape[2]]
 
-    if retmat.shape!=(targetsize_z,targetsize_y,targetsize_x):
-        print('scale3D did not return the expected size output',retmat.shape)
+    retmat = nd.interpolation.zoom(inmat, zoom=z, order=order, mode=mode, cval=cval, prefilter=prefilter)
+
+    if retmat.shape != (targetsize_z, targetsize_y, targetsize_x):
+        print('scale3D did not return the expected size output', retmat.shape)
         raise Exception('scale3D error')
 
     return retmat
@@ -36,11 +39,11 @@ def checkDixonImage(dixon_img) -> bool:
     else:
         return False
         
-def get_fmf(pattern):
-    """Get first file matching given pattern or raise an exception
+def get_fmf(pattern: str, case_sensitive = False) -> str:
+    """Get first file matching given pattern(s) or raise an exception
 
     Args:
-        pattern (str): File pattern to match
+        pattern (str/list): File pattern(s) to match
 
     Returns:
         filename: The first matching filename
@@ -48,8 +51,16 @@ def get_fmf(pattern):
     Example:
         get_fmf('/home/myfile.*')
     """
-    files = glob.glob(pattern)
-    if len(files) < 1:
-        raise Exception(f'ERROR: No files found matching {pattern}')
-    return files[0]
+    if type(pattern) is str: pattern = [pattern]
+    
+    for p in pattern:
+        if (sys.version_info.major==3 and sys.version_info.minor>=12) or sys.version_info.major>3:
+            files = [*Path.cwd().glob(p, case_sensitive=case_sensitive)]
+        else:
+            print('WARNING: Python version is < 3.12')
+            files = [*Path.cwd().glob(p)]
 
+        if len(files) > 0:
+            return str(files[0])
+
+    raise Exception(f'ERROR: No files found matching {pattern}')
